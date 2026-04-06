253d02fae85357876d28e7871daea8d6c131e1a8
Ninad Thorat <programmingwithninadthorat4@gmail.com>
2026-04-06 17:17:38 +0530

    final stable pdf fix

--- src/services/pdf.service.js
+++ src/services/pdf.service.js
@@ -1,11 +1,11 @@
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
-const pdf = require("pdf-parse");
+const pdfParse = require("pdf-parse");
 
 export const extractTextFromPDF = async (fileBuffer) => {
   try {
-    const data = await pdf(fileBuffer); // ✅ NOW pdf is a function
+    const data = await pdfParse(fileBuffer); // ✅ correct
     return data.text;
   } catch (error) {
     console.error("PDF PARSE ERROR:", error);
