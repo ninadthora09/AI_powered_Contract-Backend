e92213fbdc5804e72d77aaed43086fdf58142954
Ninad Thorat <programmingwithninadthorat4@gmail.com>
2026-04-06 16:54:40 +0530

    Done

--- src/services/pdf.service.js
+++ src/services/pdf.service.js
@@ -1,10 +1,11 @@
-import * as pdfParse from "pdf-parse";
+import pdf from "pdf-parse/lib/pdf-parse.js";
 
 export const extractTextFromPDF = async (fileBuffer) => {
   try {
-    const data = await pdfParse(fileBuffer);
+    const data = await pdf(fileBuffer);
     return data.text;
   } catch (error) {
+    console.error("PDF PARSE ERROR:", error);
     throw new Error("PDF parsing failed");
   }
 };
